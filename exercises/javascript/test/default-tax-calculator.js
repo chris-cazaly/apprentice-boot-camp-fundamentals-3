eb8e836eb37a49694bff92fa430b1f92e3cebf80
Chris Cazaly <chris.cazaly@coop.co.uk>
2022-02-01 15:11:36 +0000

    remove feature flags for story 4 and 5

--- exercises/javascript/test/default-tax-calculator.js
+++ exercises/javascript/test/default-tax-calculator.js
@@ -16,30 +16,20 @@ let DefaultTaxCalculator = class DefaultTaxCalculator extends TaxCalculator {
     const year = vehicle.dateOfFirstRegistration.getFullYear()
     const maxIntervals = [0, 50, 75, 90,  100, 110, 130, 150, 170, 190, 225,  255, 1000000]
     const petrolPrices = [0, 10, 25, 105, 125, 145, 165, 205, 515, 830, 1240, 1760, 2070]
-    const isStory4Enabled = true;
-    const isStory5Enabled = true;
-
-
-    if (isStory4Enabled) {
-      if (this.getYear() > year) {
-        if (price <= 40000) {
-          switch (fuelType) {
-            case (FuelType.PETROL):
-            case (FuelType.DIESEL):
-              return 140;
-            case (FuelType.ELECTRIC):
-              return 0;
-            case (FuelType.ALTERNATIVE_FUEL):
-              return 130;
-            default: break;
-          }
-        }
-      }
-    }
     
-
-    if (isStory5Enabled) {
-      if (this.getYear() > year) {
+    if (this.getYear() > year) {
+      if (price <= 40000) {
+        switch (fuelType) {
+          case (FuelType.PETROL):
+          case (FuelType.DIESEL):
+            return 140;
+          case (FuelType.ELECTRIC):
+            return 0;
+          case (FuelType.ALTERNATIVE_FUEL):
+            return 130;
+          default: break;
+        }
+      } else {
         if (fuelType == FuelType.PETROL || fuelType == FuelType.DIESEL) {
           return 450
         } else if (fuelType == FuelType.ELECTRIC) {
@@ -48,12 +38,12 @@ let DefaultTaxCalculator = class DefaultTaxCalculator extends TaxCalculator {
           return 440
         }
       }
-    }    
-
-      if (fuelType == FuelType.PETROL) {
-        const index = maxIntervals.findIndex(interval => co2 <= interval);
-        return petrolPrices[index];
-
+    }
+  
+    if (fuelType == FuelType.PETROL) {
+      const index = maxIntervals.findIndex(interval => co2 <= interval);
+      return petrolPrices[index];
+      
     } else if (fuelType == FuelType.ALTERNATIVE_FUEL) {
         if (co2 <= 50) return 0;
         else if (co2 <= 75) return 15
